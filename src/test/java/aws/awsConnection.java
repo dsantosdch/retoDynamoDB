1db9936519fd3cffbbf244c98513f546123b61e8
dsantosdch <dsantosd@choucairtesting.com>
2023-09-11 15:08:32 -0500

    Se añadieron los archivos del proyecto

---
+++ src/test/java/aws/awsConnection.java
@@ -0,0 +1,22 @@
+package aws;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+public class awsConnection {
+
+    private static AwsBasicCredentials getSessionCredentials() {
+        return AwsBasicCredentials.create("AKIAXA5GPF4BPUAM5CP3", "2r79FASz+uVBPO6FfvVckr80NWVy4HHcUFSWBUpe");
+    }
+
+    public static DynamoDbClient getDynamoClient() {
+        return DynamoDbClient.builder()
+                .region(Region.US_EAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(getSessionCredentials()))
+                .build();
+    }
+//comentario
+
+}
